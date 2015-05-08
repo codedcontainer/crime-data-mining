febf2c9c2e66bcef65497ef434fb76285cf630b7
JasonUtt <jutt@codedcontainer.com>
2015-05-08 19:05:28 -0400

    CURL not working with page. Must be dynamic.

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php
 error_reporting(-1);
 /*init_set('display_errors', 1);*/
-/* Get the following webpage and extract the data to be
-sent to mongo db for storage */
-$httpUrl = 'http://www.mylocalcrime.com/#39.955199%2C%20-86.11117';
+
+function get_data($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	curl_setopt($ch, CURLOPT_COOKIEFILE, 'crime_cookies.txt');
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+$httpUrl = get_data('http://www.mylocalcrime.com/#39.955199%2C%20-86.11117');
+//file_put_contents('crime_data.html', $httpUrl);
+
+
+
 $crime_doc = new DOMDocument(); 
-$crime_doc->loadHTML($httpUrl);
+echo $crime_doc;
+$crime_doc->loadHTMLFile($httpUrl);
+
+file_put_contents('crime_data.html', $crime_doc);
+
 $crime_xpath = new DOMXPath($crime_doc);
 var_dump($crime_xpath);
 
